ad5562106bc0c7a61f09618bae341a389b675e00
helo-ch <hchevalier33@gmail.com>
2017-05-25 20:41:29 +0200

    Update versionnote.cpp

--- PluriNotes/versionnote.cpp
+++ PluriNotes/versionnote.cpp
@@ -57,3 +57,44 @@ void Article::readFromFile(QXmlStreamReader &stream)
     titre = VersionNote::textNextBaliseXml(stream);
     texte = VersionNote::textNextBaliseXml(stream);
 }
+
+Tache::Tache(QXmlStreamReader& stream) : VersionNote()
+{
+    readFromFile(stream);
+}
+
+Tache::Tache(const QString& _titre,const Qstring& _action,const int _priorite,const QDateTime _echeance,Status _status = QString("En attente"), QDateTime modif = QDateTime::currentDateTime()) :
+    VersionNote(_titre,modif),action(_action),priorite(_priorite),status(_status)
+{
+        
+}
+
+Tache::~Tache()
+{
+    
+}
+
+void Tache::writeInFile(QWmlStreamWriter& stream) const
+{
+    stream.writeStartElement("Version");
+
+    stream.writeTextElement("Type", type());
+    stream.writeTextElement("Date", dateModif.toString());
+    stream.writeTextElement("Titre", titre);
+    stream.writeTextElement("Action", action);
+    stream.writeTextElement("Priorite", QString::number(priorite));
+    stream.writeTextElement("Echeance", echeance.toString());
+    stream.writeTextElement("Statut", status);
+
+    stream.writeEndElement();
+}
+
+void Tache::readFromFile(QXmlStreamReader& stream)
+{
+    dateModif = QDateTime::fromString(VersionNote::textNextBaliseXml(stream));
+    titre = VersionNote::textNextBaliseXml(stream);
+    action = VersionNote::textNextBaliseXml(stream);
+    priorite = VersionNote::textNextBaliseXml(stream).toInt();
+    echeance = QDateTime::fromString(VersionNote::textNextBaliseXml(stream));
+    status = VersionNote::textNextBaliseXml(stream);
+}
